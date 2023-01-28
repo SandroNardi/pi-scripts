25f196b661c66c6cc8bb6ecb573ae0aa99cbf586
SandroNardi <sandronardi87@gmail.com>
2023-01-28 11:02:02 +0100

    2 commit

--- sensordash-dep-docker.sh
+++ sensordash-dep-docker.sh
@@ -1,12 +1,5 @@
 
-cd ~/sensordash
-
-docker-compose stop && docker-compose rm -f
-
-cd ~
-#clone from github
-git clone git@github.com:SandroNardi/sensordash.git
-#move in directory
-cd ~/sensordash
-#docker compose
-docker compose up -d
+(cd ~/sensordash;docker-compose stop && docker-compose rm -f)
+(cd ~/;rm -rf sensordash)
+(cd ~/;git clone git@github.com:SandroNardi/sensordash.git)
+(cd ~/sensordash;docker compose up -d)
