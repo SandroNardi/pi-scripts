a04066c9b0619e219525b80851679cb3beca4e59
SandroN <51317651+SandroNardi@users.noreply.github.com>
2023-12-13 14:50:47 +0100

    Create pisetup.sh

---
+++ pisetup.sh
@@ -0,0 +1,21 @@
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt get sqlite3 xrdp -y
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker pi
+
+mkdir app
+ 
+cd app
+
+git clone https://github.com/SandroNardi/piholedocker.git
+
+docker pull pihole/pihole
+docker pull jellyfin/jellyfin
+
