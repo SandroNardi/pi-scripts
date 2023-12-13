c5a948878be22abffb36ca052d7c5e79ecf06630
SandroN <51317651+SandroNardi@users.noreply.github.com>
2023-12-13 14:51:18 +0100

    Delete sensordash-dep-docker.sh

--- sensordash-dep-docker.sh
+++
@@ -1,9 +0,0 @@
-
-echo "= STOP Container"
-(cd ~/sensordash;docker compose down -v)
-echo "= Remove directory"
-(cd ~/;rm -rf sensordash)
-echo "= Clone directory "
-(cd ~/;git clone git@github.com:SandroNardi/sensordash.git)
-echo "= Compose UP"
-(cd ~/sensordash;docker compose up -d)
