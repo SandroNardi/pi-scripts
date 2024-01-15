59c2fcee7ef2fa691fd37e3ceb8062b013d01d52
SandroN <sandronardi87@gmail.com>
2024-01-15 01:18:09 +0100

    comments

--- pi-setup.sh
+++ pi-setup.sh
@@ -1,28 +1,26 @@
+# apt update
 sudo apt update
-
 sudo apt upgrade -y
 
+# additional software
+# sqlite3 for database access for pihole docker
+# xrdp for remote desktop
 sudo apt install sqlite3 xrdp -y
 
+# install docker and permission
 curl -fsSL https://get.docker.com -o get-docker.sh
-
 sudo sh get-docker.sh
-
 sudo usermod -aG docker pi
 
+# create a folder for my app
 sudo mkdir /home/pi/app
-
 sudo chmod o+rwx /home/pi/app
- 
 cd /home/pi/app
 
-
-
-
-
-
+#git clone repos
 git clone https://github.com/SandroNardi/piholedocker.git
 
+# pulll docker images
 docker pull pihole/pihole
 docker pull jellyfin/jellyfin
 
